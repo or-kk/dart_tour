125eb1e36f20f5115d7ed626cf82387cc1e03034
tmdrl1026 <tmdrl1026@naver.com>
2023-05-12 00:25:56 +0900

    add typedef functions

---
+++ functions/Typedef .dart
@@ -0,0 +1,22 @@
+typedef ListOfInts = List<int>;
+
+ListOfInts reverseListOfNumbers(List<int> list) {
+  var reversed = list.reversed;
+  return reversed.toList();
+}
+
+typedef UserInfo = Map<String, String>;
+
+String sayHi(UserInfo userInfo) {
+  return "Hi ${userInfo['name']}";
+}
+
+void main() {
+  print(reverseListOfNumbers([
+    1,
+    2,
+    3,
+  ]));
+
+  print(sayHi({"name" : "Ralph"}));
+}
